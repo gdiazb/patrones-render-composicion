4dafa6846e8283b19d952402489b3c969b8b9aec
Geraldine Diaz B <gdiaz.contact@gmail.com>
2023-03-06 21:23:33 -0500

    add composition base

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,71 @@
-import React from "react";
-import { TodoProvider } from "./TodoConstext";
+import React, { useState } from "react";
+// import { TodoProvider } from "./TodoConstext";
 
-import { View } from './View'
-
+// import { View } from './View'
 
 function App() {
+  const [state, setState] = useState('Estado compartido')
+
   return (
-    <TodoProvider>
-      <View />
-    </TodoProvider>
-  );
+    <>
+      <TodoHeader>
+        <TodoCounter></TodoCounter>
+        <TodoSearch></TodoSearch>
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state}></TodoItem>
+      </TodoList>
+    </>
+  )
 }
+
+function TodoCounter() {
+  return (
+    <>
+      <p>TodoCounter</p>
+    </>
+  )
+}
+
+function TodoSearch() {
+  return (
+    <>
+      <p>TodoSearch</p>
+    </>
+  )
+}
+
+function TodoHeader({ children }) {
+  return (
+    <header>
+      { children }
+    </header>
+  )
+}
+
+function TodoItem({ state }) {
+  return (
+    <>
+      <p>TodoItem {state}</p>
+    </>
+  )
+}
+
+function TodoList({ children }) {
+  return (
+    <section>
+      { children }
+    </section>
+  )
+}
+
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <View />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
